a00edba7572316fa7cc3751cab08cdff1b615ad1
Nils Winkler <nils@nilswinkler.com>
2011-10-25 08:17:22 +0200

    Moved some aliases

--- aliases/available/nwinkler.aliases.bash
+++ aliases/available/nwinkler.aliases.bash
@@ -3,3 +3,13 @@ alias psg="ps -ef|grep"
 
 alias ff="find . -name"
 
+# Default to human readable figures
+alias df='df -h'
+alias du='du -h'
+
+# Misc :)
+alias less='less -r'                          # raw control characters
+alias whence='type -a'                        # where, of a sort
+alias grep='grep --color'                     # show differences in colour
+
+
