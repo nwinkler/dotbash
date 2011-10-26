2ad87560854e3f08a0554ca69a6263771956b53c
Nils Winkler <nils@nilswinkler.com>
2011-10-26 17:18:38 +0200

    Added first batch of Maven aliases

---
+++ aliases/available/maven.aliases.bash
@@ -0,0 +1,7 @@
+alias mci="mvn clean install"
+alias mi="mvn install"
+alias mrprep="mvn release:prepare"
+alias mrperf="mvn release:perform"
+alias mrrb="mvn release:rollback"
+alias mdep="mvn dependency:tree"
+
