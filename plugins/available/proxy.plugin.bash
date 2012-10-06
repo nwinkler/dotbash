b7dfdbda454c9aa7d59387e9f94338ffcd803cb1
Nils Winkler <nils@nilswinkler.com>
2012-10-06 21:02:45 +0200

    Added support for global Git proxy.

--- plugins/available/proxy.plugin.bash
+++ plugins/available/proxy.plugin.bash
@@ -7,6 +7,7 @@ disable-proxy ()
 	unset https_proxy
 	
 	_npm-disable-proxy
+	_git-global-disable-proxy
 }
 
 enable-proxy ()
@@ -15,6 +16,7 @@ enable-proxy ()
 	export https_proxy=$BASHIT_HTTPS_PROXY
 	
 	_npm-enable-proxy
+	_git-global-enable-proxy
 }
 
 show-proxy ()
@@ -24,6 +26,7 @@ show-proxy ()
 	env | grep "proxy"
 	
 	_npm-show-proxy
+	_git-global-show-proxy
 }
 
 _npm-show-proxy ()
@@ -46,6 +49,26 @@ _npm-enable-proxy ()
 	npm config set https-proxy $BASHIT_HTTPS_PROXY
 }
 
+_git-global-show-proxy ()
+{
+	echo "Git (Global Settings)"
+	echo "====================="
+	echo "Git (Global) HTTP  proxy: " `git config --global --get http.proxy`
+	echo "Git (Global) HTTPS proxy: " `git config --global --get https.proxy`
+}
+
+_git-global-disable-proxy ()
+{
+	git config --global --unset http.proxy
+	git config --global --unset https.proxy
+}
+
+_git-global-enable-proxy ()
+{
+	git config --global --add http.proxy $BASHIT_HTTP_PROXY
+	git config --global --add https.proxy $BASHIT_HTTPS_PROXY
+}
+
 # Planned additional functionality:
 # .ssh/config
-# Git proxy
+# Git proxy per project
