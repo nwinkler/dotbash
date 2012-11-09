140ac73eb6fb697201d3301d9e6c03d613ed9061
Nils Winkler <nils@nilswinkler.com>
2012-11-09 10:32:34 +0100

    Added svn_url function for printing the remote URL of a file managed in SVN.

--- plugins/available/subversion.plugin.bash
+++ plugins/available/subversion.plugin.bash
@@ -19,3 +19,17 @@ svn_add(){
 
     svn status | grep '^\?' | sed -e 's/? *//' | sed -e 's/ /\ /g' | xargs svn add
 }
+
+svn_url() {
+	about 'print the SVN URL of the specified file'
+	param '1: file to print the URL for'
+	group 'svn'
+	
+	if [ -z "$1" ]; then
+		local SVN_FILE="."
+	else
+		local SVN_FILE=$1
+	fi
+	
+	svn info "$SVN_FILE" | grep "^URL:" | sed -e 's/^URL: //g'
+}
