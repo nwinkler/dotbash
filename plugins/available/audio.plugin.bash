8ea05973bb60502dd48ad632f422a857a8123e3f
Nils Winkler <nils@nilswinkler.com>
2013-01-07 16:40:45 +0100

    Added support for decoding MP3 and AAC to WAV - untested.

---
+++ plugins/available/audio.plugin.bash
@@ -0,0 +1,26 @@
+cite about-plugin
+about-plugin 'Audio Conversion Tools'
+
+function mp3_to_wav() {
+	about 'Convert an MP3 file to a WAV file. The WAV file will be created in the current directory'
+	group 'audio'
+	
+	local orig_name=`basename "$1"`
+	local file_name=$( echo ${orig_name} | awk -F. '{ print $1 }' ).wav
+	
+	echo "Decoding to ${file_name}" 
+	
+	lame -decode ${orig_name} ./${file_name}
+}
+
+function aac_to_wav() {
+	about 'Convert an AAC (MP4, m4a) file to a WAV file. The WAV file will be created in the current directory'
+	group 'audio'
+	
+	local orig_name=`basename "$1"`
+	local file_name=$( echo ${orig_name} | awk -F. '{ print $1 }' ).wav
+	
+	echo "Decoding to ${file_name}" 
+	
+	faad -o ${file_name}.wav ./${orig_name} 
+}
